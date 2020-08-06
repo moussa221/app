28b6e476ba7f431df366841338907f1aeef026b9
moussa221 <68646181+moussa221@users.noreply.github.com>
2020-08-06 01:53:38 +0000

    Update function.php

--- function/function.php
+++ function/function.php
@@ -4,7 +4,8 @@ function bdd(){
      try
 {
 	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-	$bdd = new PDO('mysql:host=localhost;dbname=TutoForum', 'root', 'root', $pdo_options);
+	$bdd = new PDO('mysql:host=remotemysql.com;dbname=8dc7uoGEac', '8dc7uoGEac', 'Yd7flN473i', $pdo_options);
+	//$bdd = new PDO('mysql:host=localhost;dbname=TutoForum', 'root', 'root', $pdo_options);
 }
 catch (Exception $e)
 {
